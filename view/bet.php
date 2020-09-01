59fefbe42ff208463d255a5732a3f27695030693
Anou <anousone.mounivongs@gmail.com>
2020-09-01 17:00:40 +0200

    page bet

---
+++ view/bet.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Let's Bet</title>
+    <link rel="stylesheet" href="../assets/style/style.css">
+</head>
+<body>
+<?php include '../include/include_navbar.php' ?>
+<h1>LET'S BET</h1>
+
+
+
+
+
+
+<script src="https://kit.fontawesome.com/2edc250389.js" crossorigin="anonymous"></script>
+</body>
+</html>
