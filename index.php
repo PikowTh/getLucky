52d031a85fbfc682f7dcca2df27b20d936ca5697
Your Name <anousone.mounivongs@gmail.com>
2020-09-02 00:30:12 +0200

    small changes

--- index.php
+++ index.php
@@ -22,57 +22,53 @@ require_once 'controller/LoginRegex.php';
     <link rel="stylesheet" href="../assets/css/style.css">
 </head>
 
-<body class="primary-color-dark">
+<body class="bg-veine">
 
     <h1 class="text-center white-text m-5">VEINE</h1>
 
     <div class="container">
-        <div class="card light-blue darken-3">
-            <div class="card-body">
 
-                <form id="formregister" action="" method="post">
+        <form id="formregister" action="" method="post">
 
-                    <div class="row justify-content-center">
-                        <div class="col-10">
-                            <input type="mail" placeholder="mail" id="Mail" name="Mail" class="form-control" value="<?= isset($_POST['Mail']) ? htmlspecialchars(strip_tags($_POST['Mail'])) : '' ?>">
-                        </div>
-                    </div>
-                    <div class="row justify-content-center">
-                        <div class="col-10 text-center">
-                            <div style="height: 26px"><span class="font-italic white-text"><?= (isset($error['Mail'])) ? $error['Mail'] : '' ?><span></div>
-                        </div>
-                    </div>
-
-                    <div class="row justify-content-center">
-                        <div class="col-10">
-                            <input type="password" placeholder="mot de passe" id="Password" name="Password" class="form-control" value="<?= isset($_POST['Password']) ? htmlspecialchars(strip_tags($_POST['Password'])) : '' ?>">
-                        </div>
-                    </div>
-                    <div class="row justify-content-center">
-                        <div class="col-10 text-center">
-                            <span class="font-italic white-text"><?= (isset($error['Password'])) ? $error['Password'] : '' ?></span>
-                        </div>
-                    </div>
-                    <div class="row justify-content-center">
-                        <div class="col-10 text-center">
-                            <button type="submit" id="Login-submit" name="Login-submit" class="btn btn-unique btn-sm">Connexion</button>
-                        </div>
-                    </div>
-                    <div class="row justify-content-center">
-                        <div class="col-10 text-center">
-                            <span class="font-italic white-text"><?= (isset($error['login'])) ? $error['login'] : '' ?></span>
-                        </div>
-                    </div>
-                    <div class="row justify-content-center">
-                        <div class="col-10 text-center text-dark">
-                            <p>Pas encore inscrit ? C'est par<a href="view/register.php"> ici </a>!</p>
-                        </div>
-                    </div>
-
-                </form>
-
+            <div class="row justify-content-center">
+                <div class="col-10">
+                    <input type="mail" placeholder="Email" id="Mail" name="Mail" class="form-control rounded-pill" value="<?= isset($_POST['Mail']) ? htmlspecialchars(strip_tags($_POST['Mail'])) : '' ?>">
+                </div>
             </div>
-        </div>
+            <div class="row justify-content-center">
+                <div class="col-10 text-center">
+                    <div class="msg-error"><span class="font-italic white-text"><?= (isset($error['Mail'])) ? $error['Mail'] : '' ?><span></div>
+                </div>
+            </div>
+
+            <div class="row justify-content-center">
+                <div class="col-10">
+                    <input type="password" placeholder="Mot de passe" id="Password" name="Password" class="form-control rounded-pill" value="<?= isset($_POST['Password']) ? htmlspecialchars(strip_tags($_POST['Password'])) : '' ?>">
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-10 text-center">
+                    <div class="msg-error"><span class="font-italic white-text"><?= (isset($error['Password'])) ? $error['Password'] : '' ?></span></div>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-10 text-center">
+                    <button type="submit" id="Login-submit" name="Login-submit" class="btn btn-unique rounded-pill">Connexion</button>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-10 text-center">
+                    <span class="font-italic white-text"><?= (isset($error['login'])) ? $error['login'] : '' ?></span>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-10 text-center text-dark">
+                    <p>Pas encore inscrit ? C'est par<a href="view/register.php"> ici </a>!</p>
+                </div>
+            </div>
+
+        </form>
+
     </div>
 
     <!-- jQuery -->
