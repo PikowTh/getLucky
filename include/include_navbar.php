328b062299624e972cf7e24a4c311dffcefd7fc6
Anou <anousone.mounivongs@gmail.com>
2020-09-01 17:00:22 +0200

    Navbar OK pour linker vers bet

--- include/include_navbar.php
+++ include/include_navbar.php
@@ -4,7 +4,7 @@
             
             <li><a href="home.php"><i class="fas fa-home"></i></a></li>
             <li><a href=""><i class="fas fa-euro-sign"></i></a></li>
-            <li><a href=""><i class="fas fa-play"></i></a></li>
+            <li><a href="bet.php"><i class="fas fa-play"></i></a></li>
             <li><a href="contact.php"><i class="fas fa-address-book"></i></a></li>
             <li><a href="parameter.php"><i class="fas fa-cog"></i></a></li>
         </ul>
