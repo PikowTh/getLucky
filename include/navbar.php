35f9a7d547c11ca9fb2d1c23d53df480007da8b6
Your Name <anousone.mounivongs@gmail.com>
2020-09-03 01:16:38 +0200

    Mise à jour de la navbar

---
+++ include/navbar.php
@@ -0,0 +1,17 @@
+<div class="row justify-content-around text-white text-center mr-1 ml-1">
+    <div class="col nav-btn">
+        <a href="home.php" class="text-white"><i class="fas fa-home"></i></a>
+    </div>
+    <div class="col nav-btn">
+        <a href="contact.php" class="text-white"><i class="fas fa-address-book"></i></a>
+    </div>
+    <div class="col play-btn">
+        <a href="" class="text-white"><i class="fas fa-dice"></i></a>
+    </div>
+    <div class="col nav-btn">
+        <a href="bet.php" class="text-white"><i class="fas fa-coins"></i></a>
+    </div>
+    <div class="col nav-btn">
+        <a href="parameter.php" class="text-white"><i class="fas fa-cog"></i></a>
+    </div>
+</div>
