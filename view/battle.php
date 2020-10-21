d568066deafca179e44119ff7d5878a9b0d33d95
Your Name <anousone.mounivongs@gmail.com>
2020-10-22 00:26:13 +0200

    maj

--- view/battle.php
+++ view/battle.php
@@ -47,165 +47,12 @@ require_once '../controller/battleController.php';
          </div>
       </div>
 
-      <div class="stepper mt-3" data-content="step-1">
-
-         <div class="row justify-content-center">
-            <!-- title -->
-            <div class="col">
-               <p class="h5">Qui souhaites-tu affronter ?</p>
-            </div>
-<<<<<<< HEAD
-         </div>
-
-         <div class="table-responsive">
-            <table class="table">
-               <thead>
-                  <tr>
-                     <th scope="col">#</th>
-                     <th scope="col">First</th>
-                     <th scope="col">Last</th>
-                     <th scope="col">Handle</th>
-                     <th scope="col">Handle</th>
-                     <th scope="col">Handle</th>
-                     <th scope="col">Handle</th>
-                     <th scope="col">Handle</th>
-                     <th scope="col">Handle</th>
-                  </tr>
-               </thead>
-               <tbody>
-                  <tr>
-                     <th scope="row">1</th>
-                     <td>Mark</td>
-                     <td>Otto</td>
-                     <td>@mdo</td>
-                     <td>@mdo</td>
-                     <td>@mdo</td>
-                     <td>@mdo</td>
-                     <td>@mdo</td>
-                     <td>@mdo</td>
-                  </tr>
-               </tbody>
-            </table>
-         </div>
-
-         <div class="row justify-content-end">
-            <!-- buttons -->
-            <button id="who" type="button" data-who class="stepper-btn btn btn-dark mr-3">Qui</button>
-            <button type="button" data-on class="stepper-btn btn btn-dark mr-3">Sur</button>
-            <button type="button" data-what class="stepper-btn btn btn-dark mr-3">Quoi</button>
-            <button type="button" data-when class="stepper-btn btn btn-dark mr-3">Quand</button>
-            <button type="button" data-submit class="stepper-btn btn btn-dark mr-3">Go!</button>
-         </div>
-
-      </div>
-
-      <!-- permet le scroll du bas -->
-      <div class="bottom-div"></div>
-
-   </div><!-- fin container main body -->
-
-   <div class="bottom-phone elegant-color-dark fixed-bottom">
-      <?php
-      include_once '../include/navbar.php'
-      ?>
-   </div>
-
-   <!-- jQuery -->
-   <script type="text/javascript" src="../assets/js/jquery.min.js"></script>
-   <!-- Bootstrap tooltips -->
-   <script type="text/javascript" src="../assets/js/popper.min.js"></script>
-   <!-- Bootstrap core JavaScript -->
-   <script type="text/javascript" src="../assets/js/bootstrap.min.js"></script>
-   <!-- MDB core JavaScript -->
-   <script type="text/javascript" src="../assets/js/mdb.min.js"></script>
-   <!-- Sweet Alert CDN -->
-   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-
-   <!-- Your custom scripts (optional) -->
-   <script>
-      let betInformations = [];
-
-      // recupération des inputs respectifs lors du click sur le bouton next
-      $("button[data-who]").click(function() {
-         console.log('gogo');
-         betInformations[0] = 'Contre Polaire';
-      });
-      $("button[data-on]").click(function() {
-         betInformations[1] = 'Fnatic Gagne les Worlds';
-      });
-      $("button[data-what]").click(function() {
-         betInformations[2] = 'Un Kebab';
-      });
-      $("button[data-when]").click(function() {
-         betInformations[3] = '2020-05-23 16:00';
-      });
-
-      // contrôle des données avant envoi sur le bouton submit
-      $("button[data-submit]").click(function() {
-
-         let addBet = true;
-
-         // contrôles si le tableau est vide
-         if (betInformations.length == 0) {
-            addBet = false;
-            console.log('Aucun détail');
-         }
-         // contrôles si le tableau ne contient pas 'undefined'
-         if (betInformations.includes(undefined) || (betInformations.length < 4 && betInformations.length != 0)) {
-            addBet = false;
-            console.log('Attention tout n\'est pas rempli');
-         }
-
-         // envoi des données en ajax
-         if (addBet) {
-            $.ajax({
-               url: '../controller/betAjax.php',
-               type: 'GET',
-               data: {
-                  'bet': 'add',
-                  'betName': 'Worlds League of Legends',
-                  'betDescription': 'Je pari que les Fnatic gagne les worlds',
-                  'betEndTtime': '1900-01-01 00:00:00',
-                  'contactId': 22,
-                  'betType': 1
-               },
-               success: function(dataReturn) {
-                  if (dataReturn) {
-                     Swal.fire(
-                        'Good job!',
-                        'You clicked the button!',
-                        'success'
-                     );
-                  }
-               },
-               error: function() {
-                  console.log('La pari n\'a pas pu été ajouté')
-               },
-            });
-         }
-      });
-   </script>
-=======
-        </div>
-
-
         <div class="stepper mt-3" data-content="step-1">
 
             <div class="row justify-content-center">
                 <!-- title -->
                 <div class="col">
                     <p class="h5">Qui souhaites-tu affronter ?</p>
-                </div>
-            </div>
-
-            <div class="row justify-content-center">
-                <!-- content -->
-                <div class="col">
-                    <div class="card stylish-color-dark">
-                        <div class="card-body text-white">
-                            This is some text within a card body.
-                        </div>
-                    </div>
                 </div>
             </div>
 
@@ -306,7 +153,6 @@ require_once '../controller/battleController.php';
             }
         });
     </script>
->>>>>>> c97398adc6958417dda7419bf40251127b829664
 </body>
 
 </html>
