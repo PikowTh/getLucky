27e6591d7aa058190ac3d533d8b4b89c6f8cef40
Anou <anousone.mounivongs@gmail.com>
2020-10-27 12:29:03 +0100

    mise à jour

--- view/battle.php
+++ view/battle.php
@@ -7,82 +7,190 @@ require_once '../controller/battleController.php';
 <html lang="fr">
 
 <head>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Veine, votre jour de chance ?</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Veine, votre jour de chance ?</title>
 
-   <!-- Font Awesome -->
-   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
-   <!-- Google Fonts Roboto -->
-   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
-   <!-- Bootstrap core CSS -->
-   <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
-   <!-- Material Design Bootstrap -->
-   <link rel="stylesheet" href="../assets/css/mdb.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+    <!-- Google Fonts Roboto -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+    <!-- Material Design Bootstrap -->
+    <link rel="stylesheet" href="../assets/css/mdb.min.css">
 
-   <!-- Your custom styles (optional) -->
-   <link rel="stylesheet" href="../assets/css/style.css">
+    <!-- Your custom styles (optional) -->
+    <link rel="stylesheet" href="../assets/css/style.css">
 
 </head>
 
 <body class="bg-veine">
 
-   <div class="container main-body">
+    <div class="container main-body">
+
+
+        <div class="row justify-content-between bg-light">
+            <div class="col text-center">
+                <span class="stepper-badge badge unique-color-dark">1</span>
+            </div>
+            <div class="col text-center">
+                <span class="stepper-badge badge grey">2</span>
+            </div>
+            <div class="col text-center">
+                <span class="stepper-badge badge grey">3</span>
+            </div>
+            <div class="col text-center">
+                <span class="stepper-badge badge grey">4</span>
+            </div>
+            <div class="col text-center">
+                <span class="stepper-badge badge grey">GO</span>
+            </div>
+        </div>
 
 
 
-
-
-
-
-
-
-
-
-
-   
-
-      <div class="row justify-content-between">
-         <div class="col text-center">
-            <span class="stepper-badge badge unique-color-dark">1</span>
-         </div>
-         <div class="col text-center">
-            <span class="stepper-badge badge grey">2</span>
-         </div> 
-         <div class="col text-center">
-            <span class="stepper-badge badge grey">3</span>
-         </div>
-         <div class="col text-center">
-            <span class="stepper-badge badge grey">4</span>
-         </div>
-         <div class="col text-center">
-            <span class="stepper-badge badge grey">GO</span>
-         </div>
-      </div>
-
-        <div class="stepper mt-3" data-content="step-1">
+        <!-- ----------------- STEP 1 --------------------- -->
+        <!-- ---------------------------------------------- -->
+        <div id="step-1" class="mt-3" data-content="step-1">
 
             <div class="row justify-content-center">
-                <!-- title -->
+                <div class="col">
+                    
+                    <p class="h5">Qui souhaites-tu affronter ?</p>
+                    <div class="table-responsive">
+                        <table class="table">
+                            <tr>
+                                <td>COUCOU</td>
+                                <td>COUCOU</td>
+                                <td>COUCOU</td>
+                                <td>COUCOU</td>
+                                <td>COUCOU</td>
+                                <td>COUCOU</td>
+                                <td>COUCOU</td>
+                                <td>COUCOU</td>
+                                <td>COUCOU</td>
+                                <td>COUCOU</td>
+                                <td>COUCOU</td>
+                                <td>COUCOU</td>
+                            </tr>
+                        </table>
+                    </div>
+                    <div>
+                        <table class="table">
+                            <tr>
+                                <td>COUCOU</td>
+                            </tr>
+                            <tr>
+                                <td>COUCOU</td>
+                            </tr>
+                            <tr>
+                                <td>COUCOU</td>
+                            </tr>
+                            <tr>
+                                <td>COUCOU</td>
+                            </tr>
+                            <tr>
+                                <td>COUCOU</td>
+                            </tr>
+                            <tr>
+                                <td>COUCOU</td>
+                            </tr>
+                            <tr>
+                                <td>COUCOU</td>
+                            </tr>
+                            <tr>
+                                <td>COUCOU</td>
+                            </tr>
+                            <tr>
+                                <td>COUCOU</td>
+                            </tr>
+                            <tr>
+                                <td>COUCOU</td>
+                            </tr>
+                        </table>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <button id="who" type="button" data-who class="stepper-btn btn btn-dark mr-3">Qui</button>
+            <button id="btn-step-1" class="btn btn-light" type="button">step 2</button>
+
+        </div>
+
+
+
+        <!-- ----------------- STEP 2 --------------------- -->
+        <!-- ---------------------------------------------- -->
+        <div id="step-2" class="mt-3" data-content="step-2">
+
+            <div class="row justify-content-center">
                 <div class="col">
                     <p class="h5">Qui souhaites-tu affronter ?</p>
                 </div>
             </div>
 
-            <div class="row justify-content-end">
-                <!-- buttons -->
-                <button id="who" type="button" data-who class="stepper-btn btn btn-dark mr-3">Qui</button>
-                <button type="button" data-on class="stepper-btn btn btn-dark mr-3">Sur</button>
-                <button type="button" data-what class="stepper-btn btn btn-dark mr-3">Quoi</button>
-                <button type="button" data-when class="stepper-btn btn btn-dark mr-3">Quand</button>
-                <button type="button" data-submit class="stepper-btn btn btn-dark mr-3">Go!</button>
+            <button type="button" data-on class="stepper-btn btn btn-dark mr-3">Sur</button>
+            <button id="btn-step-2" class="btn btn-light" type="button">step 3</button>
+
+        </div>
+
+
+
+        <!-- ----------------- STEP 3 --------------------- -->
+        <!-- ---------------------------------------------- -->
+        <div id="step-3" class="mt-3" data-content="step-3">
+
+            <div class="row justify-content-center">
+                <div class="col">
+                    <p class="h5">Qui souhaites-tu affronter ?</p>
+                </div>
             </div>
 
+            <button type="button" data-what class="stepper-btn btn btn-dark mr-3">Quoi</button>
+            <button id="btn-step-3" class="btn btn-light" type="button">step 4</button>
+
         </div>
 
-        <div class="bottom-div">
-            <!-- permet le scroll du bas -->
+
+
+        <!-- ----------------- STEP 4 --------------------- -->
+        <!-- ---------------------------------------------- -->
+        <div id="step-4" class="mt-3" data-content="step-4">
+
+            <div class="row justify-content-center">
+                <div class="col">
+                    <p class="h5">Qui souhaites-tu affronter ?</p>
+                </div>
+            </div>
+
+            <button type="button" data-when class="stepper-btn btn btn-dark mr-3">Quand</button>
+            <button id="btn-step-4" class="btn btn-light" type="button">suivant</button>
+
         </div>
+
+
+
+        <!-- ----------------- STEP 5 --------------------- -->
+        <!-- ---------------------------------------------- -->
+        <div id="step-5" class="mt-3" data-content="step-4">
+
+            <div class="row justify-content-center">
+                <div class="col">
+                    <p class="h5">Qui souhaites-tu affronter ?</p>
+                </div>
+            </div>
+
+            <button type="button" data-submit class="stepper-btn btn btn-light mr-3">Go!</button>
+
+        </div>
+
+
+        <!-- div permettant le scroll du bas -->
+        <!-- ---------------------------------------------- -->
+        <div class="bottom-div"></div>
 
     </div><!-- fin container main body -->
 
@@ -104,7 +212,37 @@ require_once '../controller/battleController.php';
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 
     <!-- Your custom scripts (optional) -->
+
     <script>
+        $('#btn-step-1').click(function() {
+            $('#step-1').hide();
+            $('#step-2').show();
+        })
+
+        $('#btn-step-2').click(function() {
+            $('#step-2').hide();
+            $('#step-3').show();
+        })
+
+        $('#btn-step-3').click(function() {
+            $('#step-3').hide();
+            $('#step-4').show();
+        })
+
+        $('#btn-step-4').click(function() {
+            $('#step-4').hide();
+            $('#step-5').show();
+        })
+
+
+
+
+
+
+
+
+
+
         let betInformations = [];
 
         // recupération des inputs respectifs lors du click sur le bouton next
