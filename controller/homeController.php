9fa3e5fe5e7e6ed0c453fc9a2cee72a440cbcd3f
Your Name <anousone.mounivongs@gmail.com>
2020-11-03 02:16:40 +0100

    modification de la page home

---
+++ controller/homeController.php
@@ -0,0 +1,22 @@
+<?php
+
+session_start();
+
+// controle si une session user est créé
+if (!isset($_SESSION['User'])) {
+    header('Location: ../index.php');
+    exit;
+}
+
+require_once '../model/modelBets.php';
+
+$betsObj = new Bets();
+$betsArray = $betsObj->getChallengeBets();
+
+//tableau des icones des paris
+$typeOfArray = [
+    1 => "fas fa-hamburger",
+    2 => "fas fa-pizza-slice",
+    3 => "fas fa-film",
+    4 => "fas fa-beer"
+];
--- model/modelBets.php
+++ model/modelBets.php
@@ -29,7 +29,7 @@ class Bets
      */
     public function addBet($betName, $betDescription, $endTime, $contactId, $betType)
     {
-        
+
         $query = 'INSERT INTO lhp4_bets (bets_name, bets_description, bets_end_time, bets_accepted, contacts_id, users_id, bet_types_id)
         VALUES (:bets_name, :bets_description, :bets_end_time, :bets_accepted, :contacts_id, :users_id, :bet_types_id)';
 
@@ -54,16 +54,49 @@ class Bets
         }
     }
 
+    /**
+     * Fonction permettant d'obtenir tous les paris / défis créés par la personne connectée via $_SESSION
+     * @return array tableau contenant les détails du pari
+     */
     public function getAllBets()
     {
-        
+
         $query = 'SELECT bets_id, bets_name, bets_end_time, bets_accepted, bet_types_id, bets_description FROM lhp4_bets WHERE users_id = :userId';
 
         try {
 
             $resultQuery = $this->bdd->prepare($query);
             $resultQuery->bindValue(':userId', (int)($_SESSION['User']['users_id']), PDO::PARAM_INT);
-                
+
+            if ($resultQuery->execute()) {
+                return $resultQuery->fetchAll();
+            } else {
+                return false;
+            }
+        } catch (Exception $e) {
+            die('Erreur : ' . $e->getMessage());
+        }
+    }
+
+
+
+    /**
+     * Fonction permettant d'obtenir tous les paris qui nous ont été lancé : en tant qu'utilisateur connecté
+     * @return array tableau contenant les détails du pari
+     */
+    public function getChallengeBets()
+    {
+
+        $query = 'SELECT bets_id, bets_name, bets_description, lhp4_bets.users_id AS challenger, bets_end_time, bet_types_id, bets_accepted FROM lhp4_bets
+            INNER JOIN lhp4_contacts
+            ON lhp4_bets.contacts_id = lhp4_contacts.contacts_id
+            WHERE lhp4_contacts.users_id = :userId';
+
+        try {
+
+            $resultQuery = $this->bdd->prepare($query);
+            $resultQuery->bindValue(':userId', (int)($_SESSION['User']['users_id']), PDO::PARAM_INT);
+
             if ($resultQuery->execute()) {
                 return $resultQuery->fetchAll();
             } else {
