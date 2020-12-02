9e8812ed4fb4c8d2dbd6c4db2aff228db69c364e
Your Name <anousone.mounivongs@gmail.com>
2020-12-03 00:23:06 +0100

    mise à jour mineure

--- view/details.php
+++ view/details.php
@@ -30,38 +30,37 @@ require_once '../controller/homeController.php';
 <body class="bg-veine">
 
     <div class="container main-body">
-        <form action="details.php" method="get">
-            <div class="row">
-                <div class="col">
-                    <p class="text-center bet-title text-uppercase"><span class="align-middle"><i class="fas fa-crown mr-2"></i>" .. Veine .. "<i class="far fa-gem ml-2"></i></span></p>
-                </div>
+
+        <div class="row">
+            <p>BIIIIIIM</p>
+            <div class="col">
+                <p class="text-center bet-title text-uppercase"><span class="align-middle"><i class="fas fa-crown mr-2"></i>" .. Veine 22 .. "<i class="far fa-gem ml-2"></i></span></p>
             </div>
+        </div>
 
-            <div class="row">
-                <div class="col primary-color-dark shadow text-white">
-                    <p class="title-contact"><span class="icon-title"><i class="far fa-bell"></i></span>Incoming Battle !!!</p>
-                </div>
+        <div class="row">
+            <div class="col primary-color-dark shadow text-white">
+                <p class="title-contact"><span class="icon-title"><i class="far fa-bell"></i></span>Incoming Battle !!!</p>
             </div>
+        </div>
 
-            <?php foreach ($betsArray as $bet) {
-                if ($bet['bets_accepted'] == 1) {
-                    echo ' ';
-                    continue;
-                } ?>
-                <div class="row shadow m-1">
-
-                    <div class="col-2 p-2 mx-auto text-center">
-                        <i class="icon-type <?= $typeOfArray[$bet['bet_types_id']] ?>"></i>
-                    </div>
-
-                    <button name="betId" value="test" class="btn col-9 text-center p-1">
-                        <p class="bet-description-title font-weight-bold"><?= $bet['bets_name'] ?></p>
-                        <p class="bet-description text-center text-truncate"><?= $bet['bets_description'] ?></p>
-                    </button>
-
+        <?php foreach ($betsArray as $bet) {
+            if ($bet['bets_accepted'] == 1) {
+                echo ' ';
+                continue;
+            } ?>
+            <div class="row shadow m-1">
+                <div class="col-2 p-2 mx-auto text-center">
+                    <i class="icon-type <?= $typeOfArray[$bet['bet_types_id']] ?>"></i>
                 </div>
-            <?php } ?>
-        </form>
+
+                <button class="btn col-9 text-center p-1">
+                    <p class="bet-description-title font-weight-bold"><?= $bet['bets_name'] ?></p>
+                    <p class="bet-description text-center text-truncate"><?= $bet['bets_description'] ?></p>
+                </button>
+            </div>
+        <?php } ?>
+
         <div class="bottom-div">
             <!-- permet le scroll du bas -->
         </div>
